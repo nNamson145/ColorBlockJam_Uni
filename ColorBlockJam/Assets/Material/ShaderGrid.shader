7453696a12fd1695e11c2dd962299f23b62d1cc0
NamSon145 <loginson140521@gmail.com>
2025-03-16 23:30:52 +0700

    Set up game
    _Make map
    _drag object + snap grid
    _Shader material unit (GPT)

---
+++ ColorBlockJam/Assets/Material/ShaderGrid.shader
@@ -0,0 +1,55 @@
+﻿Shader "Unlit/GridShader"
+{
+    Properties
+    {
+        _GridSize ("Grid Size", Float) = 1
+        _LineThickness ("Line Thickness", Range(0.001, 0.1)) = 0.02
+        _LineColor ("Line Color", Color) = (0, 0, 0, 1)
+        _BackgroundColor ("Background Color", Color) = (1, 1, 1, 1)
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            struct appdata_t 
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f 
+            {
+                float4 vertex : SV_POSITION;
+                float3 worldPos : TEXCOORD0;
+            };
+
+            float _GridSize;
+            float _LineThickness;
+            float4 _LineColor;
+            float4 _BackgroundColor;
+
+            v2f vert (appdata_t v) 
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 gridPos = abs(frac(i.worldPos.xz * _GridSize) - 0.5);
+                float gridLine = min(gridPos.x, gridPos.y) < _LineThickness ? 1.0 : 0.0;
+
+                return lerp(_BackgroundColor, _LineColor, gridLine);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ ColorBlockJam/Assets/Scrip/DragAndDropObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class DragAndDropObject : MonoBehaviour
+{
+    Vector3 mousePosition;
+
+    private float ylock;
+
+    private float gridSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //chuyển object pos từ world sang screen
+    private Vector3 GetMousePos()
+    {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    //Nhấn lưu vị trí chênh lệch giữa chuột và object vào mousePosition
+    private void OnMouseDown()
+    {
+        ylock = transform.position.y;
+        mousePosition = Input.mousePosition - GetMousePos();
+    }
+
+    //Chuyển tọa độ chuột trên màn hình thành tọa độ thế giới và gán vào object pos
+    private void OnMouseDrag()
+    {
+        //Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+        
+        transform.position = OnSnapGrid();
+
+        
+    }
+
+    private Vector3 OnSnapGrid()
+    {
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+
+        targetPos.x = Mathf.Round(targetPos.x / gridSize) * gridSize;
+        targetPos.z = Mathf.Round(targetPos.z / gridSize) * gridSize;
+
+        targetPos = new Vector3(targetPos.x, ylock, targetPos.z);
+
+        return targetPos;
+    }
+}
